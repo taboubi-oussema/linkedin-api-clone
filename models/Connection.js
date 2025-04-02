ae2bbcf18541f3e1668ed5043e4b696fd59ec3c2
unknown <taboubioussema78@gmail.com>
2025-04-02 13:17:25 +0100

    update

---
+++ models/Connection.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const ConnectionSchema = new mongoose.Schema({
+  requester: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  recipient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "accepted", "rejected"],
+    default: "pending",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Connection", ConnectionSchema);
