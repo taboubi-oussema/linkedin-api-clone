cfe7d3c2887c6ec71d036c3daa8e3b3e9c4c514a
unknown <taboubioussema78@gmail.com>
2025-04-02 13:16:58 +0100

    update

---
+++ models/Comment.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
