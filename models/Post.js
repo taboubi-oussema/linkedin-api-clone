52ca06a08e308b79dfb066a457d61a4cfd397f36
unknown <taboubioussema78@gmail.com>
2025-04-04 14:00:23 +0100

    update

---
+++ models/Post.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  media: [
+    {
+      type: String,
+    },
+  ],
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    },
+  ],
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+  shared: {
+    original: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  },
+  privacy: {
+    type: String,
+    enum: ["public", "connections", "private"],
+    default: "public",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
